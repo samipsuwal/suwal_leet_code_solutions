8568a7e84ae5e0ec68bb0aa7ad438c5ca4f415cc
samipsuwal <samipdevsuwal@gmail.com>
2022-03-11 20:24:43 -0800

    Time: 41 ms (64.53%), Space: 13.9 MB (49.14%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -0,0 +1,58 @@
+import copy
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        print(nums)
+        if len(nums) ==1:
+            return nums[0]
+        
+        if len(nums) ==2:
+            return max(nums[0], nums[1])
+        
+        if len(nums) ==3:
+            return max(nums[0], nums[1], nums[2])
+        
+        s_nums = copy.deepcopy(nums)
+        
+        # last taken out
+        s_nums.pop()
+        #first taken out
+        nums.pop(0)
+        
+        n = len(nums)
+        
+        costs=[0]*n
+        s_costs=[0]*n
+        
+        costs[0] = nums[0]
+        s_costs[0]= s_nums[0]
+        
+        print(nums)
+        print(s_nums)
+        
+        costs[1] = max(nums[0], nums[1])
+        s_costs[1]= max(s_nums[0], s_nums[1])
+        
+        
+        for i in range(2,n):
+            costs[i] = max(nums[i]+costs[i-2], costs[i-1])
+            s_costs[i] = max(s_nums[i]+s_costs[i-2], s_costs[i-1])
+            
+        print(s_costs)
+        print(costs)
+            
+        return max(s_costs[n-1], costs[n-1])
+        
+        
+        
+        
+        
+        
+        
+        
+                
+        
+        
+        
+            
+        
+        
