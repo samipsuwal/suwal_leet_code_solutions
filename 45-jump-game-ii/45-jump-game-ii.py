957600ea37a7cf950eeb854ca631f213122924b1
samipsuwal <samipdevsuwal@gmail.com>
2022-03-12 17:17:49 -0800

    Time: 12108 ms (10.60%), Space: 20.5 MB (8.04%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,33 +1,37 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n = len(nums)
+    
+        def helper(idx, nums, memo):
 
-        if n ==1:#according to test cases
-            return 0
+            if idx >= len(nums)-1:
+                return 0
+            if nums[idx]==0:
+                return None
 
-        bestWays = [0]*n
-        bestWays[n-1]=1
+            if idx in memo:
+                return memo[idx]
 
-        counter =3
-        if n==2:
-            counter=2
+            shortestNumberOfJumps = None
 
-        for i in range(n-2, -1,-1):
-            if nums[i]==0:
-                continue
-            elif nums[i]>= n-i-1:
-                bestWays[i]=1
+            jumps = nums[idx]
+            while jumps>0:
+                steps = helper(idx+jumps, nums, memo)
+
+                if steps!= None:
+                    steps+=1
+                    if shortestNumberOfJumps == None or shortestNumberOfJumps > steps:
+                        shortestNumberOfJumps = steps
+                jumps-=1
+
+            if shortestNumberOfJumps!=None:
+                memo[idx] = shortestNumberOfJumps  
             else:
-                candidates=[]
-                jump=nums[i]
-                while jump>0:
-                    if bestWays[i+jump]>0:
-                        candidates.append(bestWays[i+jump])
+                memo[idx] =None
+            return memo[idx]
 
-                    jump-=1
-                if len(candidates)>0:
-                    candidates.sort()
-                    bestWays[i]= candidates[0]+1
-        print(bestWays)
-        return bestWays[0]
+        return helper(0, nums,{})
+    
+    
+    
+        
 
