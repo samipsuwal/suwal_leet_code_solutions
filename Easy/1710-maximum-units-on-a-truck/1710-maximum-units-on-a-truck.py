902293fa5af00ec9361810d7d6e3f322e397673e
samipsuwal <samipdevsuwal@gmail.com>
2022-03-18 19:11:16 -0700

    Time: 3103 ms (5.01%), Space: 20.6 MB (8.81%) - LeetHub

---
+++ Easy/1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        
+        
+        units = []
+        
+        for boxType in boxTypes:
+            for i in range(boxType[0]):
+                units.append(boxType[1])
+                
+        units.sort()
+        #print(units)
+        ans=0
+        for i in range(truckSize):
+            if units:
+                ans+= units.pop()
+            
+        
+        return ans
+        
+        
