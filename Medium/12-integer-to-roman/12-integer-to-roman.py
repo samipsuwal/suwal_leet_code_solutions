f83c43fa3917085d5bcdc6f23e109301fec11d4b
samipsuwal <samipdevsuwal@gmail.com>
2022-03-26 22:26:54 -0700

    Time: 129 ms (5.28%), Space: 13.9 MB (84.92%) - LeetHub

---
+++ Medium/12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,44 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        
+        #place 1, 10, 20...
+        
+        def getRomanOfDigit(digit, place):
+            
+            
+            dict = {
+                0: {1:"",2:"", 3:"", 4:""},
+                1: {1:"I",2:"X", 3:"C", 4:"M"},
+                2: {1:"II",2:"XX", 3:"CC", 4: "MM"},
+                3: {1:"III",2:"XXX", 3:"CCC", 4: "MMM"},
+                4: {1:"IV",2:"XL", 3:"CD"},
+                5: {1:"V",2:"L", 3:"D"},
+                6: {1:"VI",2:"LX", 3:"DC"},
+                7: {1:"VII",2:"LXX", 3:"DCC"},
+                8: {1:"VIII",2:"LXXX", 3:"DCCC"},
+                9: {1:"IX",2:"XC", 3:"CM"},
+                
+            }
+            
+            return dict[digit][place]
+            
+            
+            
+            
+        li =[]
+        place =1
+        while num > 0:
+            digit = num%10
+            li.append(getRomanOfDigit(digit, place))
+            place+=1
+            num = num//10
+            
+        ans=""
+        for i in reversed(li):
+            ans+= i
+            
+        return ans
+            
+            
+            
+        
