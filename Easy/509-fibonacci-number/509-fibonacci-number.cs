83f258548de8bc5239570cdcbcba4f77fe82e8d3
samipsuwal <samipdevsuwal@gmail.com>
2022-05-09 15:12:44 -0700

    Time: 36 ms (51.51%), Space: 25.2 MB (73.44%) - LeetHub

---
+++ Easy/509-fibonacci-number/509-fibonacci-number.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int Fib(int n) {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+            
+            int[] arr = new int[n+1];
+
+            arr[0] = 0;
+            arr[1] = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+
+            return arr[n];
+        
+    }
+}
