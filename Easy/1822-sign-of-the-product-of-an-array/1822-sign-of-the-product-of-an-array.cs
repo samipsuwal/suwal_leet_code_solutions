f05fc31f2fbe3b1fbaa102620422a5a37252403f
samipsuwal <samipdevsuwal@gmail.com>
2022-10-08 19:55:09 -0700

    Time: 106 ms (78.96%), Space: 39.7 MB (7.53%) - LeetHub

--- Easy/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cs
+++ Easy/1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cs
@@ -10,15 +10,13 @@ public class Solution {
             if (product <0) {
                 product = -1;
             }
+            if (product ==0 ){
+                return 0;
+            }
         }
+        return product;
         
-        if (product ==0){
-            return 0;
-        } else if (product > 0){
-            return 1;
-        } else {
-            return -1;
-        }
+     
         
     }
 }
