61ba4485c484970ba19d9111ab631e7db366f365
samipsuwal <samipdevsuwal@gmail.com>
2022-10-09 21:25:00 -0700

    Time: 240 ms (99.67%), Space: 71.7 MB (6.51%) - LeetHub

--- Easy/359-logger-rate-limiter/359-logger-rate-limiter.cs
+++ Easy/359-logger-rate-limiter/359-logger-rate-limiter.cs
@@ -1,24 +1,19 @@
 public class Logger {
     private Dictionary<string, int> dict;
-    private int lastTimeStamp;
 
     public Logger() {
         this.dict = new Dictionary<string, int>();
-        this.lastTimeStamp = 0;
     }
     
     public bool ShouldPrintMessage(int timestamp, string message) {
         // Check if message exists in Dictionary
         
         if (!this.dict.ContainsKey(message)){
-            lastTimeStamp = timestamp + 10;
-            dict.Add(message, lastTimeStamp);
-            lastTimeStamp = timestamp + 10;
+            dict.Add(message, timestamp + 10);
             return true;
         } 
         else {
             if (timestamp >= dict[message]){
-                
                 dict[message] = timestamp+ 10;
                 return true;
             }
