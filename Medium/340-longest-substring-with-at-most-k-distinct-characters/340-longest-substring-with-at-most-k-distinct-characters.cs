aa6809c283ab73ed2136ac5ebfea6e3bf8f8995e
samipsuwal <samipdevsuwal@gmail.com>
2022-10-14 23:09:42 -0700

    Time: 121 ms (44.70%), Space: 36.4 MB (88.64%) - LeetHub

--- Medium/340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cs
+++ Medium/340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cs
@@ -7,33 +7,33 @@ public class Solution {
         int j =0;
         int size = s.Length;
         int max =0;
+        int currLength =0;
         
         if (size ==1 && k ==1) {
             return 1;
         }
         dict.Add(s[i],1);
+        currLength++;
         while (i < size){
             
             while ( dict.Count <= k) {
-                max = Math.Max(max, GetValueCount(dict));
+                max = Math.Max(max, currLength);
                 j++;
                 if (j >= size) {
                     return max;
                 }
                 if (dict.ContainsKey(s[j])){
-                   // Console.WriteLine("Counter " +s[j].ToString());
                     dict[s[j]] = dict[s[j]]+1;
+                    currLength++;
                 } else {
-                    //Console.WriteLine("Adding " +s[j].ToString());
                     dict.Add(s[j],1);
+                    currLength++;
                 }
                 
             }
-            /*if (dict.ContainsKey(s[i]))
-                if (dict[s[i]] > 0) dict[s[i]] = dict[s[i]]-1;
-                else dict.Remove(s[i]);*/
             dict[s[i]] = dict[s[i]]-1;
             if (dict[s[i]] == 0) dict.Remove(s[i]);
+            currLength--;
             
             i++;
         }
@@ -43,10 +43,8 @@ public class Solution {
     public int GetValueCount(Dictionary<char, int> dict) {
         int sum =0;
         foreach (KeyValuePair<char, int> c in dict) {
-            //Console.WriteLine(c.Key.ToString()+ " " + c.Value);
             sum += c.Value;
         }
-       // Console.WriteLine(sum);
         return sum;
     }
 }
