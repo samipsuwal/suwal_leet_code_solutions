735334fda12cafcb1359eb5492824a9c87deadd3
samipsuwal <samipdevsuwal@gmail.com>
2022-10-14 23:10:28 -0700

    Time: 122 ms (42.42%), Space: 36.6 MB (62.88%) - LeetHub

--- Medium/340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cs
+++ Medium/340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.cs
@@ -1,17 +1,19 @@
 public class Solution {
     public int LengthOfLongestSubstringKDistinct(string s, int k) {
         if (k == 0) return 0;
+        int size = s.Length;
+        if (size ==1 && k ==1) {
+            return 1;
+        }
         Dictionary<char, int> dict = new ();
         
         int i =0;
         int j =0;
-        int size = s.Length;
+        
         int max =0;
         int currLength =0;
         
-        if (size ==1 && k ==1) {
-            return 1;
-        }
+        
         dict.Add(s[i],1);
         currLength++;
         while (i < size){
@@ -38,13 +40,5 @@ public class Solution {
             i++;
         }
         return max;
-    }
-    
-    public int GetValueCount(Dictionary<char, int> dict) {
-        int sum =0;
-        foreach (KeyValuePair<char, int> c in dict) {
-            sum += c.Value;
-        }
-        return sum;
     }
 }
